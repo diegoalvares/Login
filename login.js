d1e70b22ca83a474f26c56420b8b1d28cb52b986
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2025-08-10 17:23:25 -0300

    atualização
    adicionando as mensagens de erro quando algum dos campos de usuário ou da senha estiverem em branco.
    
    - adicoonada mensagem de erro se caso tudo estiver em branco.
    
    - login funcionando com usuário e senha correta

--- login.js
+++ login.js
@@ -4,9 +4,22 @@ pass = document.getElementById('pass');
 
 function passar (e){
     e.preventDefault();
-    if (user.value == 'nome' & pass.value == '1234'){
+    if (user.value == "" & pass.value == ""){
+        alert('Preencha todos os campos!');
+    }
+
+    else if (user.value == "" | pass.value == ""){
+        alert('Preencha todos os campos!');
+    }
+
+    else if (user.value == "nome" & pass.value == "1234"){
         window.location.href = 'inicial.html';
     }
+
+    else {
+        alert('Usuário ou senha incorreta! Tente Novamente.')
+    }
+
 }
 
 entrada.addEventListener('click', passar);
